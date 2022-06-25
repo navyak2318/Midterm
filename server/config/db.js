6d536bef66796dac24d5a284a8b0ed330254e18c
navyak2318 <105392297+navyak2318@users.noreply.github.com>
2022-06-25 14:05:46 -0400

    added main part

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://navya2398:Navyasiri23@cluster0.cey27x6.mongodb.net/books_store?retryWrites=true&w=majority",
+  "Secret": "someSecret"
+};
